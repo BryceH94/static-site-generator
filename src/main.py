a1de66c0aa9f7f784f7d1bcd0e5b77e2b8fc4d1d
BryceH94 <BryceHargis@outlook.com>
2024-05-16 21:22:28 -0700

    Change basic ParentNode main test

--- src/main.py
+++ src/main.py
@@ -5,17 +5,21 @@ from htmlnode import ParentNode
 def main():
 
     node = ParentNode(
-        "p",
-        [
-            LeafNode("b", "Bold text"),
-            LeafNode(None, "Normal text"),
-            ParentNode(tag="p", children=[
-                LeafNode(None, "I'm in a parent")
-            ]),
-            LeafNode("i", "italic text"),
-            LeafNode(None, "Normal text"),
-        ],
-    )
+            "p",
+            [
+                ParentNode(tag="p", children=[
+                    LeafNode(None, "I'm in a parent"),
+                    ParentNode(tag="b", children=[
+                        LeafNode(None, "I'm boldly nested in a parent"),
+                        ParentNode(tag="a", children=[
+                            LeafNode(None, "I got links baby"),
+                        ], props={"href": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}),
+                        LeafNode("p", "I'm nested with my sibling")
+                    ]),
+                ]),
+            ],
+        )
+
 
     print(node.to_html())
 
