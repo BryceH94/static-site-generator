9a01fe6944bded3aa11200ae177d3710d82588d7
BryceH94 <BryceHargis@outlook.com>
2024-05-16 21:22:46 -0700

    Add remaining ParentNode tests

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from htmlnode import ParentNode
-
+from htmlnode import LeafNode
 
 class TestParentNode(unittest.TestCase):
     def test_tag_required(self):
@@ -12,13 +12,63 @@ class TestParentNode(unittest.TestCase):
         with self.assertRaises(ValueError):
             node = ParentNode(tag="a", props={"href": "google.com", "target": "_blank"})
 
-    #TODO Test Cases
-    # Test only leafNodes
-    # Test only parentNodes, flat
-    # Test nested only parentNodes
-    # Test leafNodes and parentNodes with both parents and leafNodes within
-    # Single Parent node
-    # Single leaf node child
+    def test_leaf_nodes(self):
+        node = ParentNode(
+                "p",
+                [
+                    LeafNode("b", "Bold text"),
+                    LeafNode(None, "Normal text"),
+                    LeafNode("i", "italic text"),
+                    LeafNode(None, "Normal text"),
+                ],
+            )
+        expected_result = '<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>'
+        self.assertEqual(node.to_html(),expected_result)
+
+    def test_parent_nodes(self):
+        node = ParentNode(
+                "p",
+                [
+                    ParentNode(tag="p", children=[
+                        LeafNode(None, "I'm in a parent")
+                    ]),
+                    ParentNode(tag="b", children=[
+                        LeafNode(None, "I'm boldly in a parent")
+                    ]),
+                ],
+            )
+        expected_result = "<p><p>I'm in a parent</p><b>I'm boldly in a parent</b></p>"
+        self.assertEqual(node.to_html(),expected_result)
+
+    def test_nested_nodes(self):
+        node = ParentNode(
+                "p",
+                [
+                    ParentNode(tag="p", children=[
+                        LeafNode(None, "I'm in a parent"),
+                        ParentNode(tag="b", children=[
+                            LeafNode(None, "I'm boldly nested in a parent"),
+                            ParentNode(tag="a", children=[
+                                LeafNode(None, "I got links baby"),
+                            ], props={"href": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}),
+                            LeafNode("p", "I'm nested with my sibling")
+                        ]),
+                    ]),
+                ],
+            )
+
+        expected_result = "<p><p>I'm in a parent<b>I'm boldly nested in a parent<a href=\"https://www.youtube.com/watch?v=dQw4w9WgXcQ\">I got links baby</a><p>I'm nested with my sibling</p></b></p></p>"
+        self.assertEqual(node.to_html(),expected_result)
+
+    def test_single_parent_node(self):
+        node = ParentNode("p", children=[LeafNode(None, "Content")])
+        expected_result = "<p>Content</p>"
+        self.assertEqual(node.to_html(), expected_result)
+
+    def test_single_leaf_node(self):
+        node = LeafNode(None, "Content")
+        expected_result = "Content"
+        self.assertEqual(node.to_html(), expected_result)
 
     if __name__ == "__main__":
         unittest.main()
