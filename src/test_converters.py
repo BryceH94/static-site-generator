cf851bac7974519cd59b429647679f0b592c6e35
BryceH94 <BryceHargis@outlook.com>
2024-05-17 20:44:39 -0700

    Add remaining convertTextNode tests

--- src/test_converters.py
+++ src/test_converters.py
@@ -20,12 +20,28 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         expected_result = LeafNode("i", "Content")
         self.assertEqual(text_node_to_html_node(node), expected_result)
 
+    def test_code_node(self):
+        node = TextNode("Content", "code")
+        expected_result = LeafNode("code", "Content")
+        self.assertEqual(text_node_to_html_node(node), expected_result)
 
-    #TODO test casee
-    # code node
-    # link node
-    # img node
-    # other node (should error)
+    def test_link_node(self):
+        node = TextNode("Content", "link", "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        expected_result = LeafNode("a", "Content", props={"href": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"})
+        self.assertEqual(text_node_to_html_node(node), expected_result)
+
+    def test_img_node(self):
+        node = TextNode("Content", "image", "https://www.pngfind.com/pngs/m/7-71783_pepe-the-frog-smirk-pepe-hd-png-download.png")
+        expected_result = LeafNode("img", "", props=
+            {"src": "https://www.pngfind.com/pngs/m/7-71783_pepe-the-frog-smirk-pepe-hd-png-download.png", 
+             "alt": "Content"}
+            )
+        self.assertEqual(text_node_to_html_node(node), expected_result)
+
+    def test_invalid_text_type(self):
+        node = TextNode("Content", "bogus")
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
 
     if __name__ == "__main__":
         unittest.main()
