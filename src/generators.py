4cdb7eba88c9d0cae73e42832ee35cbc4c46b11b
BryceH94 <BryceHargis@outlook.com>
2024-06-03 21:39:55 -0700

    Add files for testing generate page later

---
+++ src/generators.py
@@ -0,0 +1,15 @@
+from converters import markdown_to_html_node
+from htmlnode import HTMLNode
+from extractors import extract_title
+
+def generate_page(from_path, template_path, dest_path):
+    
+    #Print a message to the console that says something like "Generating page from from_path to dest_path using template_path".
+    #Read the markdown file at from_path and store the contents in a variable.
+    #Read the template file at template_path and store the contents in a variable.
+    #Use your markdown_to_html_node function and .to_html() method to convert the markdown file to HTML.
+    #Use the extract_title function to grab the title of the page.
+    #Replace the {{ Title }} and {{ Content }} placeholders in the template with the HTML and title you generated.
+    #Write the new HTML to a file at dest_path. Be sure to create any necessary directories if they don't exist.
+
+    pass
